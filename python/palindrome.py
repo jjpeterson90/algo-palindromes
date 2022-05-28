e53e58ce8d216dbcc46f989e5fee4c18be35f3fb
Justin <jjpeterson90@hotmail.com>
2022-05-28 12:57:20 -0500

    complete

--- python/palindrome.py
+++ python/palindrome.py
@@ -1,2 +1,18 @@
+import re
+
 def palindrome(word):
-    # Write code here
+    word = re.sub('\W', '', str(word).lower())
+    
+    def test_palindrome(word):
+        if len(word) < 2:
+            return True
+        if word[0] == word[len(word)-1]:
+            reduced_word = word.replace(word[0], '')
+            return test_palindrome(reduced_word)
+        else:
+            return False
+        
+    return test_palindrome(word)
+    
+#print(palindrome('Race car'))
+#print(palindrome('12321'))
