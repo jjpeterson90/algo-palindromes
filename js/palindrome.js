b50805c84a648abe847fc284957f114f4cd35645
Justin <jjpeterson90@hotmail.com>
2022-05-29 14:24:12 -0500

    complete

--- js/palindrome.js
+++ js/palindrome.js
@@ -1,3 +1,11 @@
-exports.palindrome = function(word) {
-
+const palindrome = function(word) {
+    word = word.toString().replaceAll(/[^a-zA-Z0-9]/ig, '').toLowerCase();
+    if (word.length < 2) return true
+    if (word[0] === word.slice(-1)) {
+        word = word.slice(1, word.length-1);
+        return palindrome(word);
+    }
+    return false
 };
+
+module.exports = { palindrome }
